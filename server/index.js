25e7e7a0fc38daa576569d4e0f60ec60ac66214c
josephdaw <josephdaw@icloud.com>
2023-05-21 16:00:28 +0930

    adding, updating and calling records

--- server/index.js
+++ server/index.js
@@ -10,7 +10,60 @@ app.use(express.json());
 
 // Define your API routes here
 
+async function main() {
+// await prisma.item.create({
+//     data: {
+//         name: 'Apple',
+//         locations: {
+//             create: {
+//                 location: 'Fruit Section',
+//                 store: {
+//                     create: {
+//                         name: 'Marion Coles',
+//                         address: 'Level 2, Westfield Marion, SA'
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// })
+
+await prisma.item.update({
+    where: {id: '3cb8cae3-62a7-4bf7-972a-421ea024e7a3'},
+    data: {
+        locations: {
+            create: {
+                location: 'Fresh Food Section',
+                store: {
+                    create: {
+                        name: 'Coles Waradale',
+                        address: 'Diagonal Road, Waradale'
+                    }
+                }
+            }
+        }
+    }
+})
+
+const allItems = await prisma.item.findMany({
+    include: {
+        locations: true,
+    }
+})
+console.dir(allItems, {depth: null})
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
 app.listen(PORT, async () => {
-    await prisma.$connect();
-  console.log(`Server is running on http://localhost:${PORT}`);
+    // await prisma.$connect();
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
